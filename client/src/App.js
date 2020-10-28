cc5627bda3bde4288bfc8f28c1075cad9f4adc32
matthewgraham1 <mjg267106@gmail.com>
2020-10-28 16:29:33 -0400

    test

--- client/src/App.js
+++ client/src/App.js
@@ -9,10 +9,10 @@ class App extends React.Component {
   }
 
   render() {
-    return (
-      <div className="App">
-        Hello World!
-      </div>
+      return (
+	<div className="App">
+          Hello World!
+        </div>
     );
   }
 }
