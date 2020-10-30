598e3e607a2bfcb237a4a888dad6cf11785b8a3f
matthewgraham1 <mjg267106@gmail.com>
2020-10-30 16:49:13 -0400

    Add the home page and its dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,33 @@
 import React from 'react';
+import './App.css'
+import './green4lyfe.css'
+import VideoImage from './video-generic-ss-1920.png'
 
 class App extends React.Component {
-
-  render() {
-      return (
-	<div className="App">
-          Hello World!
-        </div>
-    );
-  }
+    render() {
+	return (
+    <div className="App">
+      <div>
+	Green4Lyfe image here
+      </div>
+      <button className="button login-button">
+	Log in
+      </button>
+      <div style={{position: 'absolute', bottom: '30%', left: '30%'}}>
+        <img src={VideoImage} alt="Generic video image" style={{height: '300px', width: '500px'}} />
+	<button className="button take-quiz-button">
+	  Take the quiz
+	</button>
+      </div>
+      <div style={{position: 'absolute', bottom: '10%', right: '65%'}}>
+	Take our quiz for a wonderful Green experience! We promise<br></br>
+	that we put our heart and soul into this website and the <br></br>
+	magical	green energy it will give you. I\'m talking about<br></br>
+	Shrek. We will turn you into Shrek.
+      </div>
+    </div>
+	);
+    }
 }
 
 export default App;
