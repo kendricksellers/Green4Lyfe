7159e8e83f5cdb32e7e6c6c9378c8e3c9894e367
matthewgraham1 <mjg267106@gmail.com>
2020-11-03 12:08:28 -0500

    Add login and register pages to be viewable

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import App from './App';
+import Login from './Login';
+import Register from './Register';
 
+const rootElement = document.getElementById('root')
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+      <BrowserRouter>
+	<Switch>
+	<Route exact path='/' component={App} />
+	<Route path='/login' component={Login} />
+	<Route path='/register' component={Register} />
+	</Switch>
+	</BrowserRouter>,
+    rootElement
+    
+//  <React.StrictMode>
+//  <App />
+//  </React.StrictMode>,
 );
