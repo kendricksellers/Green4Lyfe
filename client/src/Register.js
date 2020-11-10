81ccc7fd27fe60dc07496ad8787c8ae580d72dcb
Kendrick Sellers <contact@kendricksellers.me>
2020-11-10 00:53:52 -0500

    Change https requests to http and fix path

--- client/src/Register.js
+++ client/src/Register.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import './Register.css'
 import './green4lyfe.css'
 import Logo from './LOGO-Black-Scrumbags.png'
-import https from 'https'
+import http from 'http'
 import querystring from 'querystring'
 import request from 'request'
 
@@ -11,48 +11,49 @@ class Register extends React.Component {
     name = null;
     email = null;
     password = null;
-    passwordConfirm = null;
-
-
-
-
-    
-    handleSubmit(event) {
-	const data = JSON.stringify({
-	    username: this.name,
-	    password: this.password,
-	    firstName: this.email,
-	    registrationDate: Date.now()
-	})
-	const options = {
-	    hostname: 'localhost/api/users',
-	    port: 5000,
-	    method: 'POST',
-	    headers: {
-		'Content-Type': 'application/json',
-		'Content-Length': data.length
-	    },
-	    form: data
+	passwordConfirm = null;
+	
+	constructor() {
+		super();
+		this.handleSubmit = this.handleSubmit.bind(this);
 	}
-	request(options, (err, res, body) => {
-	    if (err)
-		throw err
-	    console.log(body)
-	})
+
+    handleSubmit(event) {
+		event.preventDefault();
+		const data = {
+			username: this.name,
+			password: this.password,
+			email: this.email,
+			registrationDate: Date.now()
+		}
+		const options = {
+			hostname: 'localhost',
+			path: '/api/users/',
+			port: 5000,
+			method: 'POST',
+			headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+			'Content-Length': data.length
+			}
+		}
+		/*request(options, (err, res, body) => {
+			if (err)
+			throw err
+			console.log(body)
+		})*/
 	    
-	/*
-	const request = https.request(options, res => {
-	    console.log('status: ${res.statusCode}')
-	    res.on('data', d => {
-		process.stdout.write(d)
-	    })
-	})
-	request.on('error', error => {
-	    console.error(error)
-	})
-	request.write(data)
-	request.end()
-	*/
+	
+		const request = http.request(options, res => {
+			console.log('status: ${res.statusCode}')
+			res.on('data', d => {
+				console.log(d);
+			})
+		})
+		request.on('error', error => {
+			console.error(error)
+		})
+		request.write(querystring.stringify(data))
+		request.end()
     }
     render() {
 	return (	    
@@ -73,7 +74,7 @@ class Register extends React.Component {
 		<form onSubmit={this.handleSubmit}>
 		<input type="text" value={this.email} onChange={event => this.email = event.target.value} id="E-mail" name="E-mail" placeholder="E-mail"/><br/>
 		<input type="text" value={this.name} onChange={event => this.name = event.target.value} id="Username" name="Username" placeholder="Username"/><br/>
-		<input type="password" value={this.password} onChange={event => this.name = event.target.value} id="Password" name="Password" placeholder="Password"/><br/>
+		<input type="password" value={this.password} onChange={event => this.password = event.target.value} id="Password" name="Password" placeholder="Password"/><br/>
 		<input type="password" value={this.passwordConfirm} onChange={event => this.passwordConfirm = event.target.value} id="Password" name="Password" placeholder="Re-enter password"/><br/>
 		<input type="submit" value="Register" class="button" width="100%"/>
 		</form>
--- server/tables/userModel.js
+++ server/tables/userModel.js
@@ -3,7 +3,8 @@ import mongoose from 'mongoose';
 const userModelSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true }, // Password will need to be encrypted, storing it here for the time being.
-    firstName: { type: String, required: true },
+    email: { type: String, required: true },
+    firstName: { type: String },
     lastName: { type: String },
     registrationDate: { type: Date, required: true},
     accessLevel: { type: Number, default: 0 }
