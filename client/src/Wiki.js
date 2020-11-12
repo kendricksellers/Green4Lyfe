7568fbd12ad3b7792c89549339a44f0f7381e38c
robster29 <robster29@gmail.com>
2020-11-12 04:10:34 -0500

    Wiki page can now display info from Wiki-Info.json, not finished yet

--- client/src/Wiki.js
+++ client/src/Wiki.js
@@ -1,48 +1,52 @@
 import React from 'react';
 import './Wiki.css'
 import './green4lyfe.css'
+import wikiInfo from './Wiki-Info.json';
+import { Link } from 'react-router-dom'
 import Logo from './LOGO-Black-Scrumbags.png'
 
 class Wiki extends React.Component {
-    render() {
-	return (	    
-  <div>
-    <div>
-        <img src={Logo} alt="Green4Lyfe Logo" style={{height: '200px', width: '200px'}} />
-    </div>
+  render() {
+    return (	    
+      <div>
+        <Link to='/'>
+          <img src={Logo} alt='Green4Lyfe logo' style={{height: "200px", width: "300px", position: "absolute", top: "-50px", left: "-10px"}}/>
+        </Link>
 
-    <input type="text" id="Search" name="Search" placeholder="Search"/><br/>
+        <input type="text" id="Search" name="Search" placeholder="Search" style={{position: "absolute", top: "200px"}} /><br/>
 
-    <div class="lifestyle_Side">
-      <b>Lifestyle Search Result 1<br/>
-      Stuff related to this lifestyle 1 would go here<br/>
-      Figure out where to pull up information related to this lifestyle,<br/>
-      should it be saved somewhere in the github<br/>
-      or should it be pulled from other souces?</b><br/>
-      <br/>
-      <br/>
-      <b>Lifestyle Search Result 2<br/>
-      Stuff related to this lifestyle 1 would go here<br/>
-      Figure out where to pull up information related to this lifestyle,<br/>
-      should it be saved somewhere in the github<br/>
-      or should it be pulled from other souces?</b><br/>
-      <br/>
-    </div>
+        <div class="lifestyle_Side">
+          <b>Lifestyle Search Result 1<br/></b>
+          Stuff related to this lifestyle 1 would go here<br/>
+          Figure out where to pull up information related to this lifestyle,<br/>
+          should it be saved somewhere in the github<br/>
+          or should it be pulled from other souces?<br/>
+          <br/>
+          <br/>
+          <b>Lifestyle Search Result 2<br/></b>
+          Stuff related to this lifestyle 2 would go here<br/>
+          Figure out where to pull up information related to this lifestyle,<br/>
+          should it be saved somewhere in the github<br/>
+          or should it be pulled from other souces?<br/>
+          <br/>
+        </div>
+        
+        <div class="lifestyle_Main_Title">
+          {
+            wikiInfo.CommunityGarden.map((life) => {return life.Lifestyle })
+          }
+        </div>
 
-    <div class="lifestyle_Main">
-      <b>Your current Lifestyle</b>
-      <br/>
-      <b>Stuff related to user's primary lifestyle would go here</b><br/>
-      <b>Figure out where to pull up information related to this lifestyle,</b><br/>
-      <b>should it be saved somewhere in the github or should it be pulled from other souces?</b><br/>
-      <br/>
-      <br/>
-      <b>Additionally, this section should be centered and bigger than the ones on the side</b><br/>
-      <b>and should contain more detailed information</b><br/>
+        <div class="lifestyle_Main_Blurb">
+          {
+            wikiInfo.CommunityGarden.map((life) => {return life.Blurb })
+          }
+        </div>
 
-    </div>
+        
 
-  </div> );
+      </div> 
+    );
   }
 }
 export default Wiki;
