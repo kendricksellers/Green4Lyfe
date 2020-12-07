e56e8cf46a554caf877fb20d8a6fbf3790db687f
matthewgraham1 <mjg267106@gmail.com>
2020-12-07 18:27:52 -0500

    Add Question 14

---
+++ client/src/quiz/Question14.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import './Quiz.css'
+import '../green4lyfe.css'
+import Logo from '../LOGO-Black-Scrumbags.png'
+import 'semantic-ui-css/semantic.min.css'
+import { Button } from 'semantic-ui-react'
+import { getCookie } from '../cookie.js'
+
+const prevQuestion = "13";
+const currQuestion = "14";
+const nextQuestion = "15";
+
+class QuizQuestion12 extends React.Component {
+    answered = (answer) => {
+	var date = new Date();
+	date.setTime(date.getTime() + (60 * 60 * 1000));
+	document.cookie = "q" + currQuestion + "=" + answer + "; expires=" + date.toUTCString() + ";";
+	window.location.replace('http://localhost:3000/quiz/question' + nextQuestion)
+    }
+    answeredA = () => {
+	this.answered("A");
+    }
+    answeredB = () => {
+	this.answered("B");
+    }
+
+    render() {
+	return (
+	    <div>
+	      <Link to='/'>
+		<img src={Logo} alt='Green4Lyfe logo' style={{height: "200px", width: "300px", position: "absolute", top: "-50px", left: "-10px"}}/>
+	      </Link>
+	      <div className="question-box">
+		<b>
+		  { currQuestion }. Are you interested in seeing international options?
+		</b>
+		<br/>
+		<div className="answer-a" onClick={this.answeredA}>
+		  <br/>
+		  A. Heck yeah
+		</div>
+		<div className="answer-b" onClick={this.answeredB}>
+		  <br/>
+		  B. Thanks, no thanks
+		</div>
+	      </div>
+	      <div style={{position: "absolute", top: "90%", left: "42%"}}>
+		<Link to={"/quiz/question" + prevQuestion}>
+		  <Button className="button" style={{left: "26%"}}>prev</Button>
+		</Link>
+		<Link to={"/quiz/question" + nextQuestion}>
+		  <Button className="button" style={{left: "26%"}}>next</Button>
+		</Link>
+	      </div>
+	    </div>
+	);
+    }
+}
+
+export default QuizQuestion12;
+
