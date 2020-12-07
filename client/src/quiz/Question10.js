45c224faca069b174a0e97206701b48dde4c91f7
matthewgraham1 <mjg267106@gmail.com>
2020-12-07 17:16:31 -0500

    Add Question10

---
+++ client/src/quiz/Question10.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import './Quiz.css'
+import '../green4lyfe.css'
+import Logo from '../LOGO-Black-Scrumbags.png'
+import 'semantic-ui-css/semantic.min.css'
+import { Button } from 'semantic-ui-react'
+import { getCookie } from '../cookie.js'
+
+const prevQuestion = "9";
+const currQuestion = "10";
+const nextQuestion = "11";
+
+class QuizQuestion10 extends React.Component {
+    answered = (answer) => {
+	var date = new Date();
+	date.setTime(date.getTime() + (60 * 60 * 1000));
+	document.cookie = "q" + currQuestion + "=" + answer + "; expires=" + date.toUTCString() + ";";
+	window.location.replace('http://localhost:3000/quiz/question' + nextQuestion)
+    }
+    answeredA = () => {
+	this.answered("A");
+    }
+    answeredB = () => {
+	this.answered("B");
+    }
+    answeredC = () => {
+	this.answered("C");
+    }
+    answeredD = () => {
+	this.answered("D");
+    }
+
+    render() {
+	return (
+	    <div>
+	      <Link to='/'>
+		<img src={Logo} alt='Green4Lyfe logo' style={{height: "200px", width: "300px", position: "absolute", top: "-50px", left: "-10px"}}/>
+	      </Link>
+	      <div className="question-box">
+		<b>
+		  { currQuestion }. How much family do you live with?
+		</b>
+		<br/>
+		<div className="answer-a">
+		  <br/>
+		  A. Just the spouse.
+		</div>
+		<div className="answer-b">
+		  <br/>
+		  B. 1 - 2 kids
+		</div>
+		<br/>
+		<div className="answer-c">
+		  <br/>
+		  C. 3 - 4 kids
+		</div>
+		<div className="answer-d">
+		  <br/>
+		  D. 5+ kids
+		</div>
+
+	      </div>
+	      <div style={{position: "absolute", top: "90%", left: "42%"}}>
+		<Link to={"/quiz/question" + prevQuestion}>
+		  <Button className="button" style={{left: "26%"}}>prev</Button>
+		</Link>
+		<Link to={"/quiz/question" + nextQuestion}>
+		  <Button className="button" style={{left: "26%"}}>next</Button>
+		</Link>
+	      </div>
+	    </div>
+	);
+    }
+}
+
+export default QuizQuestion10;
+
