776d9fbc674869c4c2c4c338fbab176b9ff3b6c5
matthewgraham1 <mjg267106@gmail.com>
2020-12-07 16:49:22 -0500

    Question8.js: Make prev button go to previous question

---
+++ client/src/quiz/Question8.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import './Quiz.css'
+import '../green4lyfe.css'
+import Logo from '../LOGO-Black-Scrumbags.png'
+import 'semantic-ui-css/semantic.min.css'
+import { Button } from 'semantic-ui-react'
+import { getCookie } from '../cookie.js'
+
+const prevQuestion = "6";
+const currQuestion = "7";
+const nextQuestion = "8";
+
+class QuizQuestion7 extends React.Component {
+    answered = (answer) => {
+	var date = new Date();
+	date.setTime(date.getTime() + (60 * 60 * 1000));
+	document.cookie = "q" + currQuestion + "=" + answer + "; expires=" + date.toUTCString() + ";";
+	window.location.replace('http://localhost:3000/quiz/question' + nextQuestion)
+    }
+    answeredA = () => {
+	this.answered("A");
+    }
+    answeredB = () => {
+	this.answered("B");
+    }
+    answeredC = () => {
+	this.answered("C");
+    }
+    answeredD = () => {
+	this.answered("D");
+    }
+
+    render() {
+	return (
+	    <div>
+	      <Link to='/'>
+		<img src={Logo} alt='Green4Lyfe logo' style={{height: "200px", width: "300px", position: "absolute", top: "-50px", left: "-10px"}}/>
+	      </Link>
+	      <div className="question-box">
+		<b>
+		  { currQuestion }. How do you feel about your job?
+		</b>
+		<br/>
+		<div className="answer-a">
+		  <br/>
+		  A. Can I quit now?
+		</div>
+		<div className="answer-b">
+		  <br/>
+		  B. No work, no worries.
+		</div>
+		<br/>
+		<div className="answer-c">
+		  <br/>
+		  C. I love my job!!!
+		</div>
+		<div className="answer-d">
+		  <br/>
+		  D. It pays the bills.
+		</div>
+
+	      </div>
+	      <div style={{position: "absolute", top: "90%", left: "42%"}}>
+		<Link to={"/quiz/question" + prevQuestion}>
+		  <Button className="button" style={{left: "26%"}}>prev</Button>
+		</Link>
+		<Link to={"/quiz/question" + nextQuestion}>
+		  <Button className="button" style={{left: "26%"}}>next</Button>
+		</Link>
+	      </div>
+	    </div>
+	);
+    }
+}
+
+export default QuizQuestion7;
+
