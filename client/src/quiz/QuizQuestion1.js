26593c0312d3d0fe074d693ba9047d467920966d
matthewgraham1 <mjg267106@gmail.com>
2020-12-07 14:41:07 -0500

    Add Quiz css and first question

---
+++ client/src/quiz/QuizQuestion1.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import './Quiz.css'
+import './green4lyfe.css'
+import Logo from './LOGO-Black-Scrumbags.png'
+import 'semantic-ui-css/semantic.min.css'
+import { Button } from 'semantic-ui-react'
+import AnswerA from './1a.png'
+import AnswerB from './1b.png'
+import AnswerC from './1c.png'
+import AnswerD from './1d.png'
+import { getCookie } from './cookie.js'
+
+const prevQuestion = null;
+const currQuestion = "1";
+const nextQuestion = "2";
+
+class QuizQuestion1 extends React.Component {
+
+    answered = (answer) => {
+	var date = new Date();
+	date.setTime(date.getTime() + (60 * 60 * 1000));
+	document.cookie = "q" + currQuestion + "=" + answer + "; expires=" + date.toUTCString() + ";";
+	window.location.replace('http://localhost:3000/quizquestion' + nextQuestion)
+    }
+    answeredA = () => {
+	this.answered("A");
+    }
+    answeredB = () => {
+	this.answered("B");
+    }
+    answeredC = () => {
+	this.answered("C");
+    }
+    answeredD = () => {
+	this.answered("D");
+    }
+
+    render() {
+	return (
+	    <div>
+	      <Link to='/'>
+		<img src={Logo} alt='Green4Lyfe logo' style={{height: "200px", width: "300px", position: "absolute", top: "-50px", left: "-10px"}}/>
+	      </Link>
+	      <div className="question-box">
+		<b>1. What is the most valuable thing to you?</b>
+		<br/>
+		<div className="answer-a">
+		  <img src={AnswerA} className="answer-image" onClick={this.answeredA}/>
+		  <br/>
+		  A. Time
+		</div>
+		<div className="answer-b">
+		  <img src={AnswerB} className="answer-image" onClick={this.answeredB}/>
+		  <br/>
+		  B. Money
+		</div>
+		<br/>
+		<div className="answer-c">
+		  <img src={AnswerC} className="answer-image" onClick={this.answeredC}/>
+		  <br/>
+		  C. Environment
+		</div>
+		<div className="answer-d">
+		  <img src={AnswerD} className="answer-image" onClick={this.answeredD}/>
+		  <br/>
+		  D. Comfort
+		</div>
+
+	      </div>
+	      <div style={{position: "absolute", top: "90%", left: "42%"}}>
+		<Button className="button" style={{left: "26%"}}>prev</Button>
+		<Link to={"/quizquestion" + nextQuestion}>
+		  <Button className="button" style={{left: "26%"}}>next</Button>
+		</Link>
+	      </div>
+	    </div>
+	);
+    }
+}
+
+export default QuizQuestion1;
+
