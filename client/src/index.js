f2b8ac66b7178ed59cf54e83aaa892924d6c7fc5
matthewgraham1 <mjg267106@gmail.com>
2020-12-07 15:54:11 -0500

    Add Question 6

--- client/src/index.js
+++ client/src/index.js
@@ -16,6 +16,7 @@ import QuizQuestion2 from './quiz/Question2';
 import QuizQuestion3 from './quiz/Question3';
 import QuizQuestion4 from './quiz/Question4';
 import QuizQuestion5 from './quiz/Question5';
+import QuizQuestion6 from './quiz/Question6';
 
 const rootElement = document.getElementById('root')
 ReactDOM.render(
@@ -36,6 +37,7 @@ ReactDOM.render(
 	  <Route path='/quiz/question3' component={QuizQuestion3} />
 	  <Route path='/quiz/question4' component={QuizQuestion4} />
 	  <Route path='/quiz/question5' component={QuizQuestion5} />
+	  <Route path='/quiz/question6' component={QuizQuestion6} />
 	</Switch>
       </BrowserRouter>,
     rootElement
---
+++ client/src/quiz/Question6.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import './Quiz.css'
+import '../green4lyfe.css'
+import Logo from '../LOGO-Black-Scrumbags.png'
+import 'semantic-ui-css/semantic.min.css'
+import { Button } from 'semantic-ui-react'
+import { getCookie } from '../cookie.js'
+import AnswerA from './images/6a.png'
+import AnswerB from './images/6b.png'
+import AnswerC from './images/6c.png'
+import AnswerD from './images/6d.png'
+
+const prevQuestion = "5";
+const currQuestion = "6";
+const nextQuestion = "7";
+
+class QuizQuestion6 extends React.Component {
+    answered = (answer) => {
+	var date = new Date();
+	date.setTime(date.getTime() + (60 * 60 * 1000));
+	document.cookie = "q" + currQuestion + "=" + answer + "; expires=" + date.toUTCString() + ";";
+	window.location.replace('http://localhost:3000/quiz/question' + nextQuestion)
+    }
+    answeredA = () => {
+	this.answered("A");
+    }
+    answeredB = () => {
+	this.answered("B");
+    }
+    answeredC = () => {
+	this.answered("C");
+    }
+    answeredD = () => {
+	this.answered("D");
+    }
+
+    render() {
+	return (
+	    <div>
+	      <Link to='/'>
+		<img src={Logo} alt='Green4Lyfe logo' style={{height: "200px", width: "300px", position: "absolute", top: "-50px", left: "-10px"}}/>
+	      </Link>
+	      <div className="question-box">
+		<b>
+		  { currQuestion }. What do you do to relax?
+		</b>
+		<br/>
+		<div className="answer-a">
+		  <img src={AnswerA} className="answer-image" onClick={this.answeredA}/>
+		  <br/>
+		  A. Use technology
+		</div>
+		<div className="answer-b">
+		  <img src={AnswerB} className="answer-image" onClick={this.answeredB}/>
+		  <br/>
+		  B. Read a book
+		</div>
+		<br/>
+		<div className="answer-c">
+		  <img src={AnswerC} className="answer-image" onClick={this.answeredC}/>
+		  <br/>
+		  C. Hike a trail
+		</div>
+		<div className="answer-d">
+		  <img src={AnswerD} className="answer-image" onClick={this.answeredD}/>
+		  <br/>
+		  D. Meditate
+		</div>
+
+	      </div>
+	      <div style={{position: "absolute", top: "90%", left: "42%"}}>
+		<Link to={"/quiz/question" + nextQuestion}>
+		  <Button className="button" style={{left: "26%"}}>prev</Button>
+		</Link>
+		<Link to={"/quiz/question" + nextQuestion}>
+		  <Button className="button" style={{left: "26%"}}>next</Button>
+		</Link>
+	      </div>
+	    </div>
+	);
+    }
+}
+
+export default QuizQuestion6;
+
